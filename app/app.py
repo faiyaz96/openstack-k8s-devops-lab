3378c980cb7d9289d1e059e05a33b9dfec36c9a6
Faiyaz <44235779+faiyaz96@users.noreply.github.com>
2026-05-03 18:05:33 +0300

    Initital Openstack Kubernetes Devops Lab

---
+++ app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    version = os.getenv("APP_VERSION", "v1")
+    return f"Hello from OpenStack Kubernetes DevOps Lab - {version}\n"
+
+@app.route("/health")
+def health():
+    return "OK\n"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
